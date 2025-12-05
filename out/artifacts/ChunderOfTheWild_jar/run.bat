8118123cbb0a69a53ff7956c275c0b334ce66f3c
Fergus-OF <1fergusoflynn@gmail.com>
2025-12-05 22:57:58 +0000

    Doing GUI now
    Current Features:
    All rooms set up and movable between
    Move
    teleport
    Look (both objects and npcs)
    Inventory(self)
    Take
    Drop
    Fight
    Eat mid fight
    flee the fight
    Need certain requirements to access certain rooms

---
+++ out/artifacts/ChunderOfTheWild_jar/run.bat
@@ -0,0 +1,5 @@
+@echo off
+@echo off
+"C:\Users\1ferg\.jdks\openjdk-25.0.1\bin\java.exe" --enable-native-access=javafx.graphics --module-path "C:\Users\1ferg\Downloads\openjfx-25.0.1_windows-x64_bin-sdk\javafx-sdk-25.0.1\lib" --add-modules javafx.controls,javafx.fxml -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar "ChunderOfTheWild.jar"
+pause
+pause
